4be716bff7c50df635c768e50d5db7e599a25bda
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-15 18:08:30 +0100

    installed firebase

---
+++ todo-react/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD83-Q3-x9kGDvQY5npNUz5F-oB6Xb8jqs",
+  authDomain: "to-do-app-db405.firebaseapp.com",
+  projectId: "to-do-app-db405",
+  storageBucket: "to-do-app-db405.appspot.com",
+  messagingSenderId: "811080389771",
+  appId: "1:811080389771:web:52c3ba0461a9fb3bc5b08e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
