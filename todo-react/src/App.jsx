8560343aa754a73dcb227e2bed08f7db19df1aa3
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-15 17:48:41 +0100

    Got the basic list together

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,8 +1,30 @@
+import { useState } from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+import Todo from './components/Todo'
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`
+}
+
 function App() {
+  const [todos, setTodos] = useState(['Learn React', 'Grind Codewars'])
 
   return (
-    <div>
-      
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>
+          To Do App
+        </h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="What do you need to do?" />
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+          <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
