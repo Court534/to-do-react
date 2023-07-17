25305073ec0efc01dcfa6ef15eb189f1c2e95b56
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-17 13:38:59 +0100

    Working on the logic between the front and back-end

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
 import Todo from './components/Todo'
 import { db } from './firebase'
-import { collection, query, onSnapshot } from 'firebase/firestore'
+import { collection, query, onSnapshot, updateDoc, doc } from 'firebase/firestore'
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`,
@@ -16,8 +16,14 @@ const style = {
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [input, setInput] = useState('')
+  
 
   // Create todo
+  const createTodo = async (event) => {
+    event.preventDefault(event)
+  
+  }
 
   // read todo from firebase
   useEffect(() => {
@@ -30,9 +36,14 @@ function App() {
       setTodos(todosArr)
     })
     return () => unsubscribe
-  },[])
+  }, [])
+  
   // Update todo
-
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, 'todos', todo.id), {
+      completed: !todo.completed
+    })
+  }
   // Delete todo from firebase
 
   return (
@@ -41,13 +52,13 @@ function App() {
         <h3 className={style.heading}>
           To Do App
         </h3>
-        <form className={style.form}>
-          <input className={style.input} type="text" placeholder="What do you need to do?" />
+        <form onSubmit={createTodo} className={style.form}>
+          <input value={input} onChange={(event) => setInput(event.target.value)} className={style.input} type="text" placeholder="What do you need to do?" />
           <button className={style.button}><AiOutlinePlus size={30} /></button>
         </form>
         <ul>
           {todos.map((todo, index) => (
-          <Todo key={index} todo={todo} />
+          <Todo key={index} todo={todo} toggleComplete={toggleComplete} />
           ))}
         </ul>
         <p className={style.count}>You have 2 items on your list</p>
